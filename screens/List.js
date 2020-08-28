218e9463977991511f4c94a578d35cfe3252669f
imalbert <albert.s.manuel@gmail.com>
2020-08-28 17:28:56 +0800

    add styles for user list

--- screens/List.js
+++ screens/List.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Alert, View, Text, FlatList } from 'react-native'
+import { Alert, View, Text, FlatList, StyleSheet } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage'
 
 import basicUserApi from '../api/basicUserApi'
 
 const UserItem = ({ user: { item: { email, first_name, last_name }}}) => {
   return (
-    <View>
-      <Text>{email}</Text>
-      <Text>{first_name}</Text>
-      <Text>{last_name}</Text>
+    <View style={styles.item}>
+      <Text style={styles.header}>Email</Text>
+      <Text style={styles.content}>{email}</Text>
+      <Text style={styles.header}>First name</Text>
+      <Text style={styles.content}>{first_name}</Text>
+      <Text style={styles.header}>Last name</Text>
+      <Text style={styles.content}>{last_name}</Text>
     </View>
   )
 }
@@ -40,7 +43,6 @@ const ListScreen = ({ navigation }) => {
 
   return (
     <View>
-      <Text>email</Text>
       <FlatList
         data={usersList}
         renderItem={(user) => <UserItem user={user} />}
@@ -49,5 +51,21 @@ const ListScreen = ({ navigation }) => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 10,
+    borderBottomColor: 'lightgray',
+    borderBottomWidth: 1,
+  },
+  header: {
+    fontSize: 12,
+    color: 'gray'
+  },
+  content: {
+    fontSize: 16,
+    paddingBottom: 10,
+  }
+});
 
 export default ListScreen
